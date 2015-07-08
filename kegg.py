d3a98bbb351eef7eb622863150bec224f0dffd76
Endre Bakken Stovner <endrebak@stud.ntnu.no>
2015-07-08 15:00:54 +0200

    Dataframe now returned without list

--- kegg.py
+++ kegg.py
@@ -8,6 +8,7 @@ Visit github.com/endrebak/kegg for docs and help.
 Usage:
     kegg --keggcol=KEGG --species=SPEC (--definition | --gene) [--noheader] FILE
     kegg --genecol=GENE --species=SPEC [--noheader] FILE
+    kegg --species=SPEC
 
 Arguments:
     FILE                    infile to add KEGG data to (supports piping)
@@ -16,21 +17,36 @@ Arguments:
     -s SPEC --species=SPEC  name of species
 
 Options:
-    -h --help                  show this message
-    -n --noheader              the input data does not contain a header
+    -h --help               show this message
+    -n --noheader             the input data contains a header
 """
 
 from __future__ import print_function
 
-from docopt import docopt
+from sys import stdout
+import re
 
+from pandas import DataFrame
+from Bio.KEGG import REST
+
+from docopt import docopt
+from joblib import Memory
+
+
+memory = Memory(cachedir=".joblib", verbose=0)
+
+
+@memory.cache(verbose=0)
 def get_kegg(species):
 
-    import re
-    import pandas as pd
-    from Bio.KEGG import REST
-
     kegg_list = REST.kegg_list(species)
+    rowdicts = _get_rowdicts(species, kegg_list)
+
+    return DataFrame.from_dict(rowdicts)[["pathway_id", "gene", "definition"]]
+
+
+def _get_rowdicts(species, kegg_list):
+
     clean_kegg_info = re.compile(r"{}:|\n".format(species))
     parse_kegg_info = re.compile(r"[^\t;\n]+")
 
@@ -40,15 +56,22 @@ def get_kegg(species):
         kegg_info = re.sub(clean_kegg_info, "", kegg_info)
         kegg_data = re.findall(parse_kegg_info, kegg_info)
 
-        rowdict = {"pathway_id": kegg_data[0], "genes": kegg_data[1].split(", "),
-                   "definition": kegg_data[2:]}
-        rowdicts.append(rowdict)
+        for gene in kegg_data[1].split(", "):
+            rowdict = {"pathway_id": kegg_data[0], "gene": gene,
+                       "definition": ";".join(kegg_data[2:])}
 
-    return pd.DataFrame.from_dict(rowdicts)
+            rowdicts.append(rowdict)
+
+    return rowdicts
 
 
 if __name__ == '__main__':
 
     args = docopt(__doc__)
 
-    kegg_df = get_kegg(args["--species"])
+    input_df = read_indata(args["FILE"], args["--noheader"])
+
+    input_df.to_csv(stdout, sep="\t")
+
+    # kegg_df = get_kegg(args["--species"])
+    # kegg_df.to_csv(stdout, sep="\t", index=False)
